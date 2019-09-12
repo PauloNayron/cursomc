5c7453d63cdc8a69da03e9e0cdc40fa94f3a7637
PauloNayron <paulonayron.s@hotmail.com>
2019-09-12 00:34:30 -0300

    feat: criação da Classe Produto e Associação muito para muitos

---
+++ src/main/java/com/paulonayron/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.paulonayron.cursomc.repositories;
+
+import com.paulonayron.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
