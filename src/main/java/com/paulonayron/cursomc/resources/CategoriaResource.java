9d1f5dfd4fb4a58d498eb830e6bce2842a65e22f
PauloNayron <paulonayron.s@hotmail.com>
2020-02-26 10:22:54 -0300

    Configuracao Básica de Securança

--- src/main/java/com/paulonayron/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/paulonayron/cursomc/resources/CategoriaResource.java
@@ -2,8 +2,12 @@ package com.paulonayron.cursomc.resources;
 
 import com.paulonayron.cursomc.domain.Categoria;
 import com.paulonayron.cursomc.services.CategoriaService;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,6 +20,15 @@ public class CategoriaResource {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> find(@PathVariable Integer id) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String nome;
+        if (principal instanceof UserDetails) {
+            nome = ((UserDetails)principal).getUsername();
+        } else {
+            nome = principal.toString();
+        }
+        System.out.printf("Usuário Logado" + nome);
+
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
     }
