cac557b783352de721127f24da5638c90e39b926
PauloNayron <paulonayron.s@hotmail.com>
2020-02-26 09:24:14 -0300

    Adiciona rota de insert de Categoria e dependencias do OAuth2

--- src/main/java/com/paulonayron/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/paulonayron/cursomc/resources/CategoriaResource.java
@@ -4,10 +4,7 @@ import com.paulonayron.cursomc.domain.Categoria;
 import com.paulonayron.cursomc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -21,5 +18,11 @@ public class CategoriaResource {
     public ResponseEntity<?> find(@PathVariable Integer id) {
         Categoria obj = service.buscar(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> insert (@RequestBody Categoria categoria) {
+        Categoria categoriaSalva =  service.insert(categoria);
+        return ResponseEntity.ok().body(categoriaSalva);
     }
 }
